7630078bf40f4048506b487386d0bc694ed2d1af
darksh3ll <darksh3ll.js@gmail.com>
2023-10-22 17:11:12 +0200

    wip

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-class Rectange:
-    widht = 3
-    height = 2
-
-    def calculte_area(self):
-      return self.widht + self.height
-
-
-
-z = Rectange()
-print(z.calculte_area())
+class Toolbox:
+    def __init__(self) -> None:
+        self.tools = []
+    
+    def add_tool(self,tool):
+        
+        self.tools.append(tool)
 
 
-=======
-for i in range(10):
-    print("* "*i)
->>>>>>> main
+
+
