540f503b6531bdddf356ff61311d25b067d89f0f
darksh3ll <darksh3ll.js@gmail.com>
2023-10-22 09:09:10 +0200

    wip

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-<<<<<<< HEAD
-class Rectange:
-    widht = 3
-    height = 2
-
-    def calculte_area(self):
-      return self.widht + self.height
-
-
-
-z = Rectange()
-print(z.calculte_area())
-
-
-=======
 for i in range(10):
     print("* "*i)
->>>>>>> main
