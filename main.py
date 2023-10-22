6175439d4ec7bcfe19373f9b1f5372cd21646308
darksh3ll <darksh3ll.js@gmail.com>
2023-10-22 20:28:09 +0200

    wip

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-class Parkings:
-    def __init__(self) -> None:
+import uuid
+from datetime import datetime
+
+class parking:
+    def __init__(self):
         pass
 
 
 
-class Cars:
-    def __init__(self) -> None:
-        pass
+class Car:
+    def __init__(self):
+        self.ticket_number = uuid.uuid4()
+        self.entry_hours = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+    def __str__(self):
+        return f"Car ID: {self.ticket_number} Entry Hours :{self.entry_hours}"
+
+
+a = Car()
+b= Car()
+print(a)
+print(b)
