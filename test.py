5abd286621f0bc4e8ae20334a5eda585bf105adb
darksh3ll <darksh3ll.js@gmail.com>
2023-10-29 17:42:15 +0100

    wip

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-class Palindrome:
-    @staticmethod
-    def is_palindrome(mot):
-        return mot == mot[::-1]
+import random
 
-print(Palindrome.is_palindrome("aba"))
+class Joueur:
+    def __init__(self,nom,score=0):
+        self.nom = nom
+        self.score = score
+
+
+class Game:
+    def __init__(self,joueur1,joueur2):
+        pass
+
+
+    @staticmethod
+    def lance_de():
+         return random.randint(1, 6)
+        
+
+
+joueur1 = Joueur("stephane")
+joueur2 = Joueur("Joel")
+
