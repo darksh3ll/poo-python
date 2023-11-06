6a662d8601982f5b128539059b1d064cf6e36a1c
darksh3ll <darksh3ll.js@gmail.com>
2023-11-06 18:59:26 +0100

    wip

--- test.py
+++ test.py
@@ -1,26 +1,22 @@
+from time import sleep
 
-class Animal:
-    """class animal
-    """
+# La valeur de départ.
+a = 0
 
-    def __init__(self,nom):
-        self.nom = nom
+# La direction du rebond : 1 pour l'incrément, -1 pour le décrément.
+direction = 1
 
-    def se_presenter(self):
-        print(f"Je suis un animal et je m'appelle {self.nom}.")
-
-
-class Oiseau(Animal):
-    def __init__(self, nom,peut_voler):
-        super().__init__(nom)
-        self.peut_voler = peut_voler
+# Boucle 40 fois pour faire rebondir `a` entre 0 et 20 deux fois.
+for _ in range(40):
+    # Affiche la valeur actuelle de `a`.
+    print(a)
     
-    def se_presenter(self):
-        super().se_presenter()
-        if self.peut_voler:
-            print("Je suis un oiseau et je peux voler !")
-        else:
-            print("Je suis un oiseau et je ne peux pas voler.")
-
-
-
+    # Pause de 1 seconde.
+    sleep(1)
+    
+    # Incrémente ou décrémente `a` selon la direction.
+    a += direction
+    
+    # Inverse la direction si `a` atteint 20 ou 0.
+    if a == 10 or a == 0:
+        direction *= -1
