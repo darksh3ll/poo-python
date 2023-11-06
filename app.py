4358f0e415c0b16fa1c599be93916e1b9a94cacd
darksh3ll <darksh3ll.js@gmail.com>
2023-11-06 20:17:23 +0100

    vv

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-class Student:
-    def __init__(self,age):
-        self.age = age
+class Etudiant:
+    def __init__(self,nom,prenom):
+        self.nom = nom
+        self.prenom = prenom
+        matiere = {}
 
-    def set_age(self,age):
-        self.age = age
+
+class Matiere:
+    matieres = ["math","Francais","musique"]
+    def __init__(self,nom):
+        self.nom = nom
+
+    @classmethod
+    def initialiser_matieres(cls):
+         for nom in cls.matieres:
+            setattr(cls, nom, Matiere(nom))
+      
 
 
-a = Student(12)
-print(a.age)
+Matiere.initialiser_matieres()
+
+print(Matiere.math.nom)  
